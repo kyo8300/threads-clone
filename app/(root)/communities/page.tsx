e43695fc0b5a191576ff127ae7e9976213c47ee8
kyo8300 <kametyou777@gmail.com>
2023-12-03 22:39:06 +0800

    :sparkles: feat: all features

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from "next/navigation";
-import { fetchUser, getActivity } from "@/lib/actions/user.action";
+import { fetchUser } from "@/lib/actions/user.action";
 import { currentUser } from "@clerk/nextjs";
-import Link from "next/link";
-import Image from "next/image";
+import { fetchCommunities } from "@/lib/actions/community.action";
+import CommunityCard from "@/components/cards/CommunityCard";
 
 async function Page() {
   const user = await currentUser();
@@ -10,9 +10,41 @@ async function Page() {
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
+
+  //   Fetch communities
+  const result = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
-      <h1 className="head-text mb-10">Communities</h1>
+      <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search Bar */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.communities.length === 0 ? (
+          <p className="no-result"> No communities</p>
+        ) : (
+          <>
+            {result.communities.map((community) => {
+              return (
+                <CommunityCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  bio={community.bio}
+                  members={community.members}
+                />
+              );
+            })}
+          </>
+        )}
+      </div>
     </section>
   );
 }
